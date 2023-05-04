214b409b6085b386b94bd7ba1e468566d1826e0e
afattahi1387 <afattahi1387@gmail.com>
2023-05-05 01:04:30 +0430

    added persian validations to project.

---
+++ resources/lang/fa/validation.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    'required' => 'لطفا فیلد :attribute را پر کنید',
+
+    'custom' => [
+        'attribute-name' => [
+            'rule-name' => 'custom-message',
+        ],
+    ],
+    
+    'attributes' => [
+        'username' => 'نام کاربری',
+        'password' => 'رمز عبور'
+    ],
+
+];
