f68bc84690e67f094c1d5626d37ea27bace962e4
afattahi1387 <afattahi1387@gmail.com>
2023-05-04 22:00:03 +0430

    added dashboard to project.

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'DashboardController@dashboard')->name('dashboard');
 
 Route::get('/login', 'MainController@login')->name('login');
