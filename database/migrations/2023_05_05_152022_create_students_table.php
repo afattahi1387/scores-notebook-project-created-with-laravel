e3dc54f2b32883a75dfef66fd0129a547a2d8a70
afattahi1387 <afattahi1387@gmail.com>
2023-05-05 19:59:35 +0430

    added students table migration and model to project.

---
+++ database/migrations/2023_05_05_152022_create_students_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->unsignedBigInteger('lesson_room_id');
+            $table->foreign('lesson_room_id')->references('id')->on('lesson_rooms')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
